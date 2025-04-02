8adbb49971aa597db8e786a63c1876cf79a3a03d
Cauê Felippe Martin <cauecauw@gmail.com>
2025-04-02 20:54:32 -0300

    aula 2 - poo

--- aula nova/aula.js
+++ aula nova/aula.js
@@ -1,16 +1,23 @@
 const prompt = require('prompt-sync')();
 // Modelo atributos e funçoes para criaçao de um objeto
 class Cliente {
-    nome; //
+    #nome; //Atributo
     idade;
     cpf;
     agencia;
+
+    getNome(){
+        return this.#nome;
+    }
+    setNome(value){
+        this.#nome = value;
+    }
 }
 
 // objeto que possu as informaçoes da classe de frma individual
 let cliente = new Cliente();
 
-cliente.nome = prompt ("digite o nome do cliente: ")
+cliente.setNome(prompt("digite o nome do cliente: "))
 cliente.idade = 23;
 cliente.cpf = "134.492.239-37";
 cliente.agencia = 102030;
